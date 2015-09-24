8b7e41aa70903fdcd6353001fb520bec3afbc9fc
Mourad Arnout <marnout@free.fr>
2015-09-24 11:03:59 +0200

    20150924

--- 4th.cpp
+++ 4th.cpp
@@ -4,6 +4,7 @@ File : 4th.cpp
 To compile you must install libreadline :
 	# apt install libreadline-gplv2-dev 
 	$ g++ 4th.cpp -std=c++14 -lreadline -Wfatal-errors -o 4th
+http://www.grognon.net
 */
 
 #include "4th.h"
